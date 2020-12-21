ce3b9ee63bb64d3adf170d5da77f9cfb25884a44
diogocorigo <diogoacorigo@gmail.com>
2020-12-21 15:36:37 +0000

    go commit die 2

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ session_start();
       src="https://code.jquery.com/jquery-3.5.1.js"
       integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
       crossorigin="anonymous"
-    ></script>
+    ></script> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
     <!-- CSS -->
